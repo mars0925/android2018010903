6eb61869a0f9583980cd28d4cf68c727c83475c5
Teacher <shaojung@gmail.com>
2018-01-08 13:22:49 +0800

    Initial commit

---
+++ app/src/main/java/tw/com/pcschool/dd2018010804/MainActivity.java
@@ -0,0 +1,46 @@
+package tw.com.pcschool.dd2018010804;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    ListView lv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        lv = (ListView) findViewById(R.id.listView);
+        MyAdapter adapter = new MyAdapter();
+        lv.setAdapter(adapter);
+    }
+    class MyAdapter extends BaseAdapter
+    {
+
+        @Override
+        public int getCount() {
+            return 5;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View view, ViewGroup viewGroup) {
+            TextView tv = new TextView(MainActivity.this);
+            tv.setText("Hello " + position);
+            return tv;
+        }
+    }
+}

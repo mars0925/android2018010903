062f2b7370315c33f4b2b5496cdb2e0d5795e889
Teacher <shaojung@gmail.com>
2018-01-08 15:17:09 +0800

    use it as simple adapter

--- app/src/main/java/tw/com/pcschool/dd2018010804/MainActivity.java
+++ app/src/main/java/tw/com/pcschool/dd2018010804/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -87,7 +89,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
         @Override
-        public View getView(int position, View view, ViewGroup viewGroup) {
+        public View getView(final int position, View view, ViewGroup viewGroup) {
             LayoutInflater inflater = LayoutInflater.from(MainActivity.this);
             Log.d("GetView", "position:" + position);
             View v = inflater.inflate(R.layout.myitem, null);
@@ -98,6 +100,14 @@ public class MainActivity extends AppCompatActivity {
             tv2.setText(mylist.get(position).get("code").toString());
             ImageView img = v.findViewById(R.id.imageView);
             img.setImageResource((Integer) mylist.get(position).get("img"));
+            CheckBox chk = (CheckBox) v.findViewById(R.id.checkBox);
+            chk.setChecked(chks[position]);
+            chk.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                    chks[position] = b;
+                }
+            });
             return v;
         }
     }

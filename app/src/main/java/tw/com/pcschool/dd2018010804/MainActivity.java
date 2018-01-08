d89276c6f348ba7b79638cec163846744d96fdf4
Teacher <shaojung@gmail.com>
2018-01-08 14:35:34 +0800

    use it as simple adapter

--- app/src/main/java/tw/com/pcschool/dd2018010804/MainActivity.java
+++ app/src/main/java/tw/com/pcschool/dd2018010804/MainActivity.java
@@ -6,17 +6,41 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
     ListView lv;
-    String str[] = {"AA", "BB", "CCC", "DDDD", "EE"};
-
+    ArrayList<Map<String, Object>> mylist = new ArrayList();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        HashMap<String, Object> m1 = new HashMap<>();
+        m1.put("city", "台北");
+        m1.put("code", "02");
+        m1.put("img", R.drawable.tpe);
+        mylist.add(m1);
+        HashMap<String, Object> m2 = new HashMap<>();
+        m2.put("city", "台中");
+        m2.put("code", "04");
+        m2.put("img", R.drawable.tc);
+        mylist.add(m2);
+        HashMap<String, Object> m3 = new HashMap<>();
+        m3.put("city", "台南");
+        m3.put("code", "06");
+        m3.put("img", R.drawable.tn);
+        mylist.add(m3);
+        HashMap<String, Object> m4 = new HashMap<>();
+        m4.put("city", "高雄");
+        m4.put("code", "07");
+        m4.put("img", R.drawable.kh);
+        mylist.add(m4);
         lv = (ListView) findViewById(R.id.listView);
         MyAdapter adapter = new MyAdapter();
         lv.setAdapter(adapter);
@@ -26,7 +50,7 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public int getCount() {
-            return str.length;
+            return mylist.size();
         }
 
         @Override
@@ -45,7 +69,11 @@ public class MainActivity extends AppCompatActivity {
             View v = inflater.inflate(R.layout.myitem, null);
 
             TextView tv = v.findViewById(R.id.textView);
-            tv.setText(str[position]);
+            tv.setText(mylist.get(position).get("city").toString());
+            TextView tv2 = v.findViewById(R.id.textView2);
+            tv2.setText(mylist.get(position).get("code").toString());
+            ImageView img = v.findViewById(R.id.imageView);
+            img.setImageResource((Integer) mylist.get(position).get("img"));
             return v;
         }
     }

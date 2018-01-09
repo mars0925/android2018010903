12692017381e99bf0b057a3fc72d083561de2013
MarsChang <mars09250148@yahoo.com.tw>
2018-01-09 11:20:55 +0800

    baseAdapter

--- app/src/main/java/tw/com/pcschool/dd2018010804/MainActivity.java
+++ app/src/main/java/tw/com/pcschool/dd2018010804/MainActivity.java
@@ -29,46 +29,47 @@ public class MainActivity extends AppCompatActivity {
         HashMap<String, Object> m1 = new HashMap<>();
         m1.put("city", "台北");
         m1.put("code", "02");
-        m1.put("img", R.drawable.tpe);
+        m1.put("img", R.drawable.tpi);
         mylist.add(m1);
         HashMap<String, Object> m2 = new HashMap<>();
         m2.put("city", "台中");
         m2.put("code", "04");
-        m2.put("img", R.drawable.tc);
+        m2.put("img", R.drawable.tac);
         mylist.add(m2);
         HashMap<String, Object> m3 = new HashMap<>();
         m3.put("city", "台南");
         m3.put("code", "06");
-        m3.put("img", R.drawable.tn);
+        m3.put("img", R.drawable.tan);
         mylist.add(m3);
         HashMap<String, Object> m4 = new HashMap<>();
         m4.put("city", "高雄");
         m4.put("code", "07");
-        m4.put("img", R.drawable.kh);
+        m4.put("img", R.drawable.hk);
         mylist.add(m4);
 
         HashMap<String, Object> m5 = new HashMap<>();
         m5.put("city", "台北2");
         m5.put("code", "202");
-        m5.put("img", R.drawable.tpe);
+        m5.put("img", R.drawable.tpi);
         mylist.add(m5);
         HashMap<String, Object> m6 = new HashMap<>();
         m6.put("city", "台中2");
         m6.put("code", "204");
-        m6.put("img", R.drawable.tc);
+        m6.put("img", R.drawable.tac);
         mylist.add(m6);
         HashMap<String, Object> m7 = new HashMap<>();
         m7.put("city", "台南2");
         m7.put("code", "206");
-        m7.put("img", R.drawable.tn);
+        m7.put("img", R.drawable.tan);
         mylist.add(m7);
         HashMap<String, Object> m8 = new HashMap<>();
         m8.put("city", "高雄2");
         m8.put("code", "207");
-        m8.put("img", R.drawable.kh);
+        m8.put("img", R.drawable.hk);
         mylist.add(m8);
         lv = (ListView) findViewById(R.id.listView);
-        MyAdapter adapter = new MyAdapter(MainActivity.this, mylist);
+       //new 自己寫的MyAdapter 傳入建構式參數
+        MyAdapter adapter = new MyAdapter(MainActivity.this, mylist,chks);
         lv.setAdapter(adapter);
     }
     public void click1(View v)
--- app/src/main/java/tw/com/pcschool/dd2018010804/MyAdapter.java
+++ app/src/main/java/tw/com/pcschool/dd2018010804/MyAdapter.java
@@ -18,14 +18,22 @@ import java.util.Map;
  * Created by Student on 2018/1/9.
  */
 
+//把MyAdapter extends BaseAdapter寫在分開寫,寫成單獨的class的時候
+/*要改哪裡
+    1.public MyAdapter(Context context, ArrayList<Map<String, Object>> mylist)
+    2. LayoutInflater inflater = LayoutInflater.from(context);
+    好處是可以不同人來寫比較方便
+     */
 public class MyAdapter extends BaseAdapter {
     ArrayList<Map<String, Object>> mylist = new ArrayList();
-    boolean chks[] = new boolean[8];
+    boolean chks[] ;
     Context context;
-    public MyAdapter(Context context, ArrayList<Map<String, Object>> mylist)
+    //建構式
+    public MyAdapter(Context context, ArrayList<Map<String, Object>> mylist,boolean chks[])
     {
         this.context = context;
         this.mylist = mylist;
+        this.chks = chks;
     }
     @Override
     public int getCount() {
@@ -43,28 +51,36 @@ public class MyAdapter extends BaseAdapter {
     }
 
     @Override
+    //讓getView不需要一直去findViewById的動作    增加效能
     public View getView(final int position, View v, ViewGroup viewGroup) {
         ViewHolder viewHolder;
+        //當v這個view是不存在的時候,表示是第一次產生的時候
         if (v == null)
         {
+            //把context這個view解出來
             LayoutInflater inflater = LayoutInflater.from(context);
             v = inflater.inflate(R.layout.myitem, null);
+            //new ViewHolder 來用
             viewHolder = new ViewHolder();
+            //下面用的元件都是從ViewHolder拿來的
             viewHolder.tv = v.findViewById(R.id.textView);
             viewHolder.tv2 = v.findViewById(R.id.textView2);
             viewHolder.img = v.findViewById(R.id.imageView);
             viewHolder.chk = (CheckBox) v.findViewById(R.id.checkBox);
+            //把已經設定好的viewHolder 放到setTag裡面 之後可以再用
+            v.setTag(viewHolder);
         }
+        //如果 v這個view存在的話,表示是回收再用的view
         else
         {
+            //就直接將tag裡面的來用
             viewHolder = (ViewHolder) v.getTag();
         }
-
-
 
         viewHolder.tv.setText(mylist.get(position).get("city").toString());
         viewHolder.tv2.setText(mylist.get(position).get("code").toString());
         viewHolder.img.setImageResource((Integer) mylist.get(position).get("img"));
+        viewHolder.chk.setOnCheckedChangeListener(null);
         viewHolder.chk.setChecked(chks[position]);
         viewHolder.chk.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
@@ -72,9 +88,11 @@ public class MyAdapter extends BaseAdapter {
                 chks[position] = b;
             }
         });
-        v.setTag(viewHolder);
+
+
         return v;
     }
+    //產生一個class放重複使用的view
     static class ViewHolder
     {
         TextView tv;

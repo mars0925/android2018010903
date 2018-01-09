b9c1909ce413f40fd71bf8b577da9058170b359b
Teacher <shaojung@gmail.com>
2018-01-09 09:23:21 +0800

    put Adapter to another class

--- app/src/main/java/tw/com/pcschool/dd2018010804/MainActivity.java
+++ app/src/main/java/tw/com/pcschool/dd2018010804/MainActivity.java
@@ -68,7 +68,7 @@ public class MainActivity extends AppCompatActivity {
         m8.put("img", R.drawable.kh);
         mylist.add(m8);
         lv = (ListView) findViewById(R.id.listView);
-        MyAdapter adapter = new MyAdapter();
+        MyAdapter adapter = new MyAdapter(mylist);
         lv.setAdapter(adapter);
     }
     public void click1(View v)
@@ -81,46 +81,7 @@ public class MainActivity extends AppCompatActivity {
                    sb.append(mylist.get(i).get("city") + ",");
             }
         }
-        Toast.makeText(MainActivity.this, sb.toString(), Toast.LENGTH_LONG).show();    }
-    class MyAdapter extends BaseAdapter
-    {
-
-        @Override
-        public int getCount() {
-            return mylist.size();
-        }
-
-        @Override
-        public Object getItem(int i) {
-            return null;
-        }
-
-        @Override
-        public long getItemId(int i) {
-            return 0;
-        }
-
-        @Override
-        public View getView(final int position, View view, ViewGroup viewGroup) {
-            LayoutInflater inflater = LayoutInflater.from(MainActivity.this);
-            Log.d("GetView", "position:" + position);
-            View v = inflater.inflate(R.layout.myitem, null);
-
-            TextView tv = v.findViewById(R.id.textView);
-            tv.setText(mylist.get(position).get("city").toString());
-            TextView tv2 = v.findViewById(R.id.textView2);
-            tv2.setText(mylist.get(position).get("code").toString());
-            ImageView img = v.findViewById(R.id.imageView);
-            img.setImageResource((Integer) mylist.get(position).get("img"));
-            CheckBox chk = (CheckBox) v.findViewById(R.id.checkBox);
-            chk.setChecked(chks[position]);
-            chk.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-                @Override
-                public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                    chks[position] = b;
-                }
-            });
-            return v;
-        }
+        Toast.makeText(MainActivity.this, sb.toString(), Toast.LENGTH_LONG).show();
     }
+
 }
